b997a64a3e55a39ad8ca29273d8f1560ac75399f
Leonel Mamani <leomamaniinca@frba.utn.edu.ar>
2022-09-19 20:37:25 -0300

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(multer({
+    dest: process.cwd() + '/files'
+}).single('upfile'))
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',(req,res)=>{
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
